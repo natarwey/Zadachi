d3aa5f592a9e5781324fe20eafbc2981cc5268f0
user <user@DESKTOP-RA6T6M6>
2024-05-11 18:40:30 +0300

    10 не правильный, 12, 14, 17 не доделан

--- Zadachi/Series.cs
+++ Zadachi/Series.cs
@@ -107,19 +107,88 @@
 //10. Дано целое число N и набор из N целых чисел. Если в наборе имеются положительные числа, то вывести TRUE;
 //в противном случае вывести FALSE.
 
+//Console.Write($"Введите число N: ");
+//int n = int.Parse(Console.ReadLine());
+//int number = 0;
+//bool product = true;
+//for (int i = 1; i <= n; i++)
+//{
+//    Console.Write($"Введите {i} число: ");
+//    number = int.Parse(Console.ReadLine());
+//    while (number > 0)
+//    {
+//        product = true;
+//    }
+//}
+//Console.WriteLine(product);
+
+//if (number > 0)
+//{
+//    Console.WriteLine("TRUE");
+//}
+//else
+//{
+//    Console.WriteLine("FALSE");
+//}
+
+
+//12. Дан набор ненулевых целых чисел; признак его завершения — число 0. Вывести количество чисел в наборе.
+
+//int num = 1, quantity = 0;
+//while (num != 0)
+//{
+//    Console.Write("Введите число: ");
+//    num = int.Parse(Console.ReadLine());
+//    quantity++;
+//}
+//Console.WriteLine(quantity);
+
+
+//14. Дано целое число K и набор ненулевых целых чисел; признак его завершения — число 0.
+//Вывести количество чисел в наборе, меньших K.
+
+//Console.Write($"Введите число K: ");
+//int k = int.Parse(Console.ReadLine());
+//int num = 1, quantity = 0;
+//while (num != 0)
+//{
+//    Console.Write("Введите число: ");
+//    num = int.Parse(Console.ReadLine());
+//    if (num < k)
+//        quantity++;
+//}
+//Console.WriteLine(quantity);
+
+
+//?17. Дано вещественное число B, целое число N и набор из N вещественных чисел, упорядоченных по возрастанию.
+//Вывести элементы набора вместе с числом B, сохраняя упорядоченность выводимых чисел.
+
+Console.Write($"Введите число B: ");
+double b = double.Parse(Console.ReadLine());
 Console.Write($"Введите число N: ");
 int n = int.Parse(Console.ReadLine());
-int number = 0;
+double num = 0;
+string s = ;
 for (int i = 1; i <= n; i++)
 {
     Console.Write($"Введите {i} число: ");
-    number = int.Parse(Console.ReadLine());
+    num = double.Parse(Console.ReadLine());
+    while (b > num)
+    {
+        
+    }
+    b = Convert.ToString(b);
+    s = num + " " + b;
+    b = Convert.ToDouble(b);
+    //if (b < num)
+    //{
+    //    Console.WriteLine($"{num} {numbers}");
+    //}
+    //else
+    //{
+    //    Console.WriteLine($"{numbers} {num}");
+    //}
 }
-if (number > 0)
-{
-    Console.WriteLine("TRUE");
-}
-else
-{
-    Console.WriteLine("FALSE");
-}
+Console.WriteLine(s);
+
+
