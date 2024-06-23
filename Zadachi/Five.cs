3e4b0c3c461c0d71f6aa6eef32e46d180048c6f7
user <user@DESKTOP-RA6T6M6>
2024-06-23 15:25:07 +0300

    примеры на 4, 5

---
+++ Zadachi/Five.cs
@@ -0,0 +1,230 @@
+﻿// наследование - ключевой прицнцип ООП, заключается в том, что позволяет одному классу унаследовать функциональность другого
+
+using System.Drawing;
+
+People tom = new("Tom", 19, 293);
+tom.Print();
+Employee bob = new("Bob", 20, 224);
+bob.Print();
+
+class People
+{
+    public string name;
+    public int age;
+    private int cvv;
+
+    public People(string name, int age, int cvv)
+    {
+        this.name = name;
+        this.age = age;
+        this.cvv = cvv;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Зовут {name}, возраст - {age}, CVV-код {cvv}");
+    }
+}
+
+class Employee : People
+{
+    public Employee(string name, int age, int cvv) : base(name, age, cvv)
+    {
+
+    }
+}
+
+//||\\
+
+//перегрузка методов позволяет создавать один и тот же метод с различными параметрами; осуществляется через сигнатуры
+
+class Calculator
+{
+    public void Sub(int x, int y) //сигнатура - Sub(int, int)
+    {
+        int result = x - y;
+    }
+    public void Sub(int x, int y, int z) //сигнатура - Sub(int, int, int), количество значений другое, потому создается перегрузка
+    {
+        int result = x - y - z;
+    }
+    public void Sub(float x, float y) //сигнатура - Sub(float, float), типы данных значений другое, поэтому создается перегрузка
+    {
+        float result = x - y;
+    }
+    public void Div(int x, decimal y) { int result = (int)(x / y); } //сигнатура - Div(int, decimal)
+    public void Div(decimal x, int y) { int result = (int)(x / y); } //сигнатура - Div(decimal, int), порядок разных типов данных другой, потому создается перегрузка
+}
+
+//||\\
+
+//свойства обеспечивают доступ к полям класс классов и структур, узнать их значение и/или выполнить их установку
+
+Car car = new Car();
+
+car.speed = 60;
+int carSpeed = car.speed;
+
+int numOfWheels = car.NumOfWheels;
+
+car.NumOfPassangers = 4;
+
+Console.WriteLine(carSpeed);
+Console.WriteLine(numOfWheels);
+
+class Car
+{
+    public int numOfWheels = 4;
+    public int numOfPassangers;
+    public int speed //в это свойство можно внести значение при помощи set и прочитать с помощью get
+    {
+        get { return speed; }
+        set { speed = value; }
+    }
+    public int NumOfWheels { get { return numOfWheels; } } //это свойство можно только прочитать, оно уже задано в классе
+    public int NumOfPassangers { set { numOfPassangers = value; } } //в это свойство можно только внести значение
+}
+
+//||\\
+
+//абстрактные классы необходимы для описания таких классов, которые будут иметь между собой что-то общее,
+//но, чтобы создать экземпляр класса, НЕ используется конструктор абстрактного класса 
+//абстрактными также могут быть методы, свойства, индексаторы и события и в таком случае они требуют перезаписи
+
+Figure rec = new Rec("Прямоугольник");
+Figure tri = new Tri("Треугольник");
+Figure cir = new Cir("Круг");
+
+rec.Print();
+tri.Print();
+cir.Print();
+abstract class Figure
+{
+    public string Name { get; }
+    public Figure(string name) { Name = name; }
+    public abstract void Print();
+}
+
+class Rec : Figure
+{
+    public Rec(string name) : base(name) { }
+    public override void Print()
+    {
+        Console.WriteLine($"У {Name} 4 угла"); ;
+    }
+}
+class Tri : Figure
+{
+    public Tri(string name) : base(name) { }
+    public override void Print()
+    {
+        Console.WriteLine($"У {Name} 3 угла"); ;
+    }
+}
+class Cir : Figure
+{
+    public Cir(string name) : base(name) { }
+    public override void Print()
+    {
+        Console.WriteLine($"У {Name} нет углов"); ;
+    }
+}
+
+//||\\
+
+//делегаты лямбды события
+
+//делегатом является объект, который указывает на метод
+
+//лямбда-выражения позволяют сокращать и упрощать запись анонимных методов, которые могут возвращать какие-то значения и которые можно передавать в качестве параметров в другие методы
+//в лямбды необязательно передавать типы данных, если не используется неявная типизация (var), то есть, например - Op sum = (x, y) => Console.WriteLine($"{x + y}")
+
+Supply sup; //делегату объявляется переменная
+sup = Send; //переменной присваетвается адрес метода
+sup(); //метод вызывается при помощи делегата
+
+void Send() => Console.WriteLine("Отправлено");
+
+delegate void Supply(); //создается делегает
+
+//события сигнализируют системе, что произошло какое-либо действие; события используются для того, чтобы отследить эти
+//действия и являются указателями на делегаты и использует заданный в делегат тип данных как выводные данные
+
+class Company
+{
+    public delegate void Product(string selled); //создается делегат
+    public event Product? IsSelled; //к делегату определяется событие с "?", не допускающий null значения
+    public Company(int sum) => Sum = sum;
+    public int Sum { get; set; }
+    public void Sell(int sum)
+    {
+        Sum += sum;
+        IsSelled?.Invoke($"Продукт был продан за {sum}"); //событие вызывается и выводит строку, т.к. в делегате указан string
+    }
+}
+
+
+//||\\
+
+//паттерн fabric позволяет облегчить создание классов, у которых совпадают 
+
+static class ProductFabric
+{
+    public static Product Create(string name, int weight, int cost)
+    {
+        return new Product(name, weight, cost);
+    }
+}
+
+class Product
+{
+    public int weight;
+
+    public string Name;
+    public int Cost;
+    public Product(string name, int weight, int cost)
+    {
+        Name = name;
+        this.weight = weight;
+        Cost = cost;
+    }
+}
+
+//||\\
+
+//интерфейс является как бы родительским классом, он определяет без реализации методы, свойства, события, статические поля, константы и индексаторы для того, чтобы
+//классы и структуры использовали и реализовывали весь функционал, применяя интерфейс
+
+interface ICat
+{
+    const int ChromosomePairs = 19;
+    string CoatColor { get; set; }
+    void Meow();
+}
+
+class BritishShortHair : ICat
+{
+    private string color;
+    public string CoatColor { get { return color; } set { color = value; } }
+    public void Meow() => Console.WriteLine("Meow (a normal one)");
+}
+
+class Lion : ICat
+{
+    private string color;
+    public string CoatColor { get { return color; } set { color = value; } }
+    public void Meow() => Console.WriteLine("MReow (an angry one)");
+}
+
+//||\\
+
+//обобщение облегчает работу с типами данных, не прибегая к использованию преобразований и проверок,
+//если мы не уверены, какие именно типы данных будут использоваться или если метод будет работать одинаково с любым типом данных
+
+void Swap<TAny>(TAny x, TAny y, TAny z) //в <> скобках указывается либо тип данных, либо универсальный тип (как сейчас)
+{
+    TAny temp = x; //такая программа будет работать с любым типом данных, будь то string, int или bool
+    x = y;
+    y = z;
+    z = temp;
+}
---
+++ Zadachi/Four.cs
@@ -0,0 +1,211 @@
+﻿//литералы переменные константы типы данных
+//литералы - неизменяемые значения, которые передаются переменным
+
+using Store;
+using System;
+
+Console.WriteLine(15);
+Console.WriteLine('a');
+Console.WriteLine(true);
+Console.WriteLine("popa");
+
+//переменные являются именнованными областями памяти, которые содержат значение определенного типа
+
+int a = 15;
+int decrement;
+string name;
+name = "vladimir";
+bool result = false;
+double Result = 3.5;
+
+//константой является переменная, которая обязательно должна быть инициализированна и остаётся неизменной 
+
+const string CATNAME = "popa";
+
+//тип данных определяет множество значений, которые может принимать переменная, и действия, которые можно совершать с переменной
+
+bool int byte short int long float double decimal char string object var;
+
+//||\\
+
+//циклы массивы списки конструкции if else switch case
+
+//циклы позволяют выполнять какое-либо действие множество раз
+//for выполняет инструкцию пока его внутренний цикл работает
+//foreach же будет выполняться столько раз, сколько какой-то объект находится в списке или массиве
+
+for (int i = 0; i < 10; i++) { Console.WriteLine(i); }
+foreach (int num in nums) { Console.WriteLine(num); }
+
+//массив - набор однотипных данных фиксированного количества
+
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+string[,] words = { { "p", "o" }, { "p", "a" } };
+
+//список тот же набор однотипных объектов
+
+var pets = new List<string>(10) { "dog", "cat", "parrot" };
+var petShop = new List<string>(pets) { "mice" };
+
+//if else
+
+int num1 = 5;
+int num2 = 77;
+
+if (num1 < num2)
+{
+    Console.WriteLine(num2);
+}
+else if (num1 > num2)
+{
+    Console.WriteLine(num1);
+}
+else { Console.WriteLine(num1 + num2); }
+
+// switch case 
+
+int w = Convert.ToInt32(Console.ReadLine());
+
+switch (w)
+{
+    case 1:
+        Console.WriteLine(w);
+        break;
+    case 2:
+        Console.WriteLine(w);
+        break;
+    case 3:
+        Console.WriteLine(w);
+        break;
+    case 4:
+        Console.WriteLine(w);
+        break;
+    default:
+        Console.WriteLine("undefined");
+        break;
+}
+
+//функции хранят в себе набор инструкций, которые выполняют действие при вызове самой функции
+
+void SayNum(int num) //здесь в функцию передано int значение - параметр; это значит, что при вызове функции в нее будет необходимо указать соответствующее значение дабы она сработала
+{
+    Console.WriteLine(num); //сама инструкция
+}
+
+void SayWord(string word) //то же самое, но параметр уже string
+{
+    Console.WriteLine(word);
+}
+
+void KeepSilence() //данная функция не требует входных параметров при её вызове, потому она просто выполнит инстркцию
+{
+    Console.WriteLine("you have nothing to say");
+}
+
+SayNum(20);
+SayWord("mew");
+KeepSilence();
+
+//||\\
+
+//классы объекты классов конструктор класса пространство имён
+
+//класс является описанием объектов этого класса, как бы шаблон
+
+//объектом класса считается экземпляр, созданный по шаблону класса
+
+//конструктор класса - специальный метод, который называется как и класс; позволяет создавать и инициализировать объекты классов
+
+//namespace позволяет отделять и объединять различные части кода и его функциональность, схожие одной темой, в специальные логические блоки
+
+using Store;
+
+Furniture wardrobe = new Furniture();
+
+int wardrobeHeight = wardrobe.height;
+int wardrobeWeight = wardrobe.weight;
+bool isOpenable = wardrobe.openable;
+
+wardrobe.height = 2;
+wardrobe.weight = 40;
+wardrobe.openable = true;
+
+wardrobe.open();
+
+namespace Store
+{
+    class Furniture
+    {
+        public int height;
+        public int weight;
+
+        public bool openable;
+
+        public void open()
+        {
+            if (openable)
+            {
+                Console.WriteLine("Дверца открыта");
+            }
+            else { Console.WriteLine("Не может быть открыта"); }
+        }
+    }
+}
+
+//||\\
+
+//статические члены и модификаторы доступа
+
+//статический член относятся ко всем классу или структуре и не требуют создания класса или структуры для того, чтобы обратиться к ним
+
+//модификатор доступа, в каком месте кода и файле может использоваться переменная или метод
+//(private, private protected, internal, internal protected, protected, public (табличку сами учите))
+
+Person pluh = new(15);
+
+int personsAge = pluh.age;
+
+class Person
+{
+    public int age;
+    private int cardcvv;
+
+    public static bool isExtrasense = false;
+
+    public Person(int age) { this.age = age; } //возможен и static конструктор
+
+    public void Print() { Console.WriteLine($"Этому человеку {age} лет"); }
+    public static void Check()
+    {
+        if (!isExtrasense)
+        {
+            Console.WriteLine("Самый обычный человек");
+        }
+        else { Console.WriteLine("Вас обманули"); }
+    }
+}
+
+//||\\
+
+//преобразование типов необходима для ситуаций, когда некоторые операции в конечном результате дают отличный типа данных от изначальных
+
+long zr = 15;
+long b = zr + 15; //данный код выдаст ошибку, так как конечный результат будет int, а не long, для этого надо:
+
+short br = 15;
+short z = (short)(br + 15);
+
+//||\\
+
+//конструкция try catch позволяет обрабатывать исключения и вылавливать ошибки
+
+try
+{
+    int dec = 5;
+    int div = dec / 0;
+    Console.WriteLine($"{div}");
+}
+catch
+{
+    Console.WriteLine("Возникло исключение");
+}
